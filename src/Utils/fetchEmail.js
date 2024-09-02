70eb7f4706469f8fa96b4432f27b2044ddca37c7
Aditya <aditya.kumar@nativebyte.in>
2024-09-02 19:27:39 +0530

    extra text remove

--- src/Utils/fetchEmail.js
+++ src/Utils/fetchEmail.js
@@ -4,16 +4,26 @@ export const fetchEmail = async (fullHtml) => {
   let emails = [];
   const emailRegex =
     /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(com|net|org|edu|gov|mil|int|co|biz|info|io|me|tv|name|pro|us|uk|au|ca|de|fr|jp|ru|cn|es|it|nl|se|no|fi|dk|ch|at|be|pt|br|za|mx|ar|cl|nz|sg|hk|my|ph|id|ae|sa|tr|pl|xyz|tech|online|site|store|app|club|design|ai|cloud|guru|space|life|world|solutions|media|agency|news|academy|capital|care|video|marketing|digital)/g;
+
   const $ = cheerio.load(fullHtml);
+
+  // Extract emails from mailto links
   $('a[href^="mailto:"]').each((i, element) => {
-    const email = $(element).attr("href").replace("mailto:", "");
+    let email = $(element).attr("href").replace("mailto:", "");
+
+    if (email.includes("?")) {
+      email = email.split("?")[0];
+    }
+
     emails.push(email);
-    emails = [...new Set(emails)];
   });
 
-  if (emails.length === 0) {
-    emails = fullHtml.match(emailRegex) || [];
-  }
+  // Extract emails from the plain text of the HTML
+  const matchedEmails = fullHtml.match(emailRegex) || [];
+  emails = emails.concat(matchedEmails);
+
+  // Remove duplicates
   emails = [...new Set(emails)];
+
   return emails;
 };
